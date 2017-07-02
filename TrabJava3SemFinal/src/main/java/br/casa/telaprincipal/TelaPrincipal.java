a09065f6d77593f90639ce643de4e4718e496ab0
Eduardo <Eduardo@Eduardo-PC>
2017-07-02 11:41:46 -0300

    Mudando de repositorio

--- TrabJava3SemFinal/src/main/java/br/casa/telaprincipal/TelaPrincipal.java
+++ TrabJava3SemFinal/src/main/java/br/casa/telaprincipal/TelaPrincipal.java
@@ -19,9 +19,6 @@ import java.awt.Insets;
 import javax.swing.JTabbedPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
 
 public class TelaPrincipal extends JFrame {
 
